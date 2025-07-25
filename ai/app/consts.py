98af34ebd2c710680a6c5b036fcea71c4635d1ff
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2025-07-25 16:21:59 +0700

    persist data of ollama
    Signed-off-by: Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>

--- ai/app/consts.py
+++ ai/app/consts.py
@@ -4,19 +4,12 @@ from pathlib import Path
 
 BASE_DIR = Path(os.getcwd())
 
-DATABASE_URL = os.getenv("POSTGRES_URL", os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"))
-
-ADMIN_USERS = os.getenv("ADMIN_USERS", "").split(",")
-
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-OAUTH_GOOGLE_CLIENT_ID = os.getenv("OAUTH_GOOGLE_CLIENT_ID")
-OAUTH_GOOGLE_CLIENT_SECRET = os.getenv("OAUTH_GOOGLE_CLIENT_SECRET")
-CHAINLIT_AUTH_SECRET = os.getenv("CHAINLIT_AUTH_SECRET")
+QDRANT_URL = os.getenv("QDRANT_URL", "http://qdrant:6333")
+
+OLLAMA_BASE_URL= os.getenv("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
 
 GRAFANA_URL: str = os.getenv("GRAFANA_URL", "http://host.docker.internal:3000")
 GRAFANA_API_KEY = os.getenv("GRAFANA_API_KEY")
 
-OLLAMA_BASE_URL= os.getenv("OLLAMA_BASE_URL", "http://host.docker.internal:11434")
-
-QDRANT_URL = os.getenv("QDRANT_URL", "http://qdrant:6333")
--- ai/app/main.py
+++
@@ -1,5 +0,0 @@
-from chainlit.utils import mount_chainlit
-
-from .api import app
-
-# mount_chainlit(app, 'app/chainlit.py', '/chainlit')
